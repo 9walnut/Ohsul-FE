63421ad297f27346994bee9998a21c51edbb76fd
dntjd129 <dntjd129@naver.com>
2024-01-31 18:45:42 +0900

    🏗️ 폴더 구조 조정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import "./App.css";
-import { Reset } from "styled-reset";
+import styled from "styled-components";
+import { FontTest } from "./components/FontTest";
+import KakaoMap01 from "./components/KakaoMap01";
 
 function App() {
   return (
     <>
-      <Reset />
-      폰트 먹었냐
+      <CenterLayout>
+        <MainLayout>
+          <KakaoMap01 />
+        </MainLayout>
+      </CenterLayout>
     </>
   );
 }
+
+const MainLayout = styled.div`
+  width: 100vw;
+  max-width: 430px;
+  height: 100vh;
+  background-color: #f4ede6;
+  text-align: center;
+`;
+
+const CenterLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </>
+  // {/* </React.StrictMode> */}
 );
 
 // If you want to start measuring performance in your app, pass a function
