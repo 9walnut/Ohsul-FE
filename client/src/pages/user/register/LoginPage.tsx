223beb49d5c356a8b142256314d7dcceb68e5d45
hyeoonjeoong <jeooooong22@gmail.com>
2024-02-11 04:05:02 +0900

    🎨 코드 스타일링

--- client/src/pages/user/register/LoginPage.tsx
+++ client/src/pages/user/register/LoginPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Header from "../../../components/common/Header";
+import RoundButton from "../../../components/common/RoundButton";
+import WideButton from "../../../components/common/WideButton";
 
-const LoginPage = () => {
+const LoginPage: React.FC = () => {
+  const handleClick = () => {
+    console.log("버튼 클릭");
+  };
+
   return (
     <>
       <Header title="로그인" />
+      <RoundButton onClick={handleClick}>버튼</RoundButton>
+      <div> 버튼</div>
+      <WideButton onClick={handleClick}>와이드버튼</WideButton>
     </>
   );
 };
