36258437af8b591517c72097fa0c8993dbb18d80
hyeoonjeoong <jeooooong22@gmail.com>
2024-02-12 02:52:23 +0900

    ✨ InputFieldText 컴포넌트

--- client/src/components/common/InputFieldText.tsx
+++ client/src/components/common/InputFieldText.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 
 interface InputProps {
   onChange: (value: string) => void;
+  children: React.ReactNode;
   name?: string;
   id: string;
   type: "text" | "number" | "password";
@@ -16,6 +18,7 @@ const InputFieldText: React.FC<InputProps> = ({
   placeholder,
   value,
   onChange,
+  children,
 }: InputProps) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onChange(event.target.value);
@@ -23,11 +26,63 @@ const InputFieldText: React.FC<InputProps> = ({
 
   return (
     <>
-      <div>InputText</div>
-      <label htmlFor={id}></label>
-      <input id={id} type={type} placeholder={placeholder} value={value} />
+      <InputLayout>
+        <StyledLabel htmlFor={id}>{children}</StyledLabel>
+        <InputFieldBox>
+          <StyledInput
+            id={id}
+            type={type}
+            placeholder={placeholder}
+            value={value}
+          />
+        </InputFieldBox>
+      </InputLayout>
     </>
   );
 };
 
 export default InputFieldText;
+
+const InputLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: 4px;
+
+  width: 343px;
+  height: 57px;
+`;
+
+const StyledLabel = styled.label`
+  font-weight: 500;
+  font-size: 12px;
+`;
+
+const InputFieldBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 16px;
+  gap: 8px;
+
+  width: 222px;
+  height: 29px;
+
+  background: #fcfaf9;
+  border-radius: 50px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  outline: none;
+  width: 222px;
+  height: 29px;
+  background-color: transparent;
+  &::placeholder {
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 24px;
+    text-align: center;
+  }
+`;
