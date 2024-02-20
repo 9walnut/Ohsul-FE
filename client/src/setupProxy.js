4117b218f2a57000d4d4c8ff8a2b8e44d1b73cd1
dntjd129 <dntjd129@naver.com>
2024-02-20 14:55:47 +0900

    ♻️ axios 수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,14 +2,14 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api/register", {
+    createProxyMiddleware("/register", {
       target: "http://localhost:8080",
       changeOrigin: true,
     })
   );
 
   app.use(
-    createProxyMiddleware("/api/login", {
+    createProxyMiddleware("/login", {
       target: "http://localhost:8080",
       changeOrigin: true,
     })
