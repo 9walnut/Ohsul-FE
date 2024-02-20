a249d7d4af1024280064d9677e2e98ffd6265780
dntjd129 <dntjd129@naver.com>
2024-02-20 14:45:52 +0900

    ♻️ axios

---
+++ client/src/setupProxy.js
@@ -0,0 +1,17 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api/register", {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    createProxyMiddleware("/api/login", {
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    })
+  );
+};
