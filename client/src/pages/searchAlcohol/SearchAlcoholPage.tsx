1966ea109bf4d9ef0fc0b17805ad12756ecdb14b
hyeoonjeoong <jeooooong22@gmail.com>
2024-02-22 02:53:05 +0900

    ✨ SearchAlcoholPage 기능 분리, ui 조정

--- client/src/pages/searchAlcohol/SearchAlcoholPage.tsx
+++ client/src/pages/searchAlcohol/SearchAlcoholPage.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/common/Header";
 import styled from "styled-components";
 import KakaoMap07 from "../../components/common/KakaoMap07";
 import TagBox from "../../components/ohsulTag/TagBox";
+import { SearchResult } from "../../types/Map";
+import CardColReview from "../../components/common/CardColReview";
 
 const SearchAlcoholPage: React.FC = () => {
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
+  const handleSearchResults = (results: SearchResult[]) => {
+    setSearchResults(results);
+  };
+
   //오술태그 선택된 값 넣기
   const DUMMYTags = {
     alcohol: ["alcohol_1", "alcohol_2", "alcohol_5"],
@@ -17,11 +25,24 @@ const SearchAlcoholPage: React.FC = () => {
   return (
     <>
       <Header title="오늘의 술 찾기" />
-      <KakaoMap07 />
+      <KakaoMap07 onSearchResults={handleSearchResults} />
       <TagBox checkedTags={DUMMYTags} />
       {/* dot Img */}
       <img src="/assets/images/border_dot.png" alt="border_dot" />
-      CardCol
+      {/* <ul>
+        {searchResults.map((result, index) => (
+          <li key={index} style={{ marginBottom: "10px" }}>
+            <strong>{result.name}</strong>
+            <br />
+            {result.address}
+            <br />
+            {result.phone}
+          </li>
+        ))}
+      </ul> */}
+      {searchResults.map((result, index) => (
+        <CardColReview barName={result.name} key={index} />
+      ))}
     </>
   );
 };
