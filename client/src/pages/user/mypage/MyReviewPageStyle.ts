3f3b221595f2e915269e2640aeff790aba48516a
hyeoonjeoong <jeooooong22@gmail.com>
2024-02-22 02:59:35 +0900

    💄 mypage layout

---
+++ client/src/pages/user/mypage/MyReviewPageStyle.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const MyReviewPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 15px;
+  gap: 4px;
+
+  position: absolute;
+
+  width: 382px;
+  height: 300px;
+
+  top: 320px;
+
+  background-color: bisque;
+`;
