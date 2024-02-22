981ec9d7ab1b617a2b6cabbe95623b52beb90b6a
hyeoonjeoong <jeooooong22@gmail.com>
2024-02-22 18:40:45 +0900

    ✨ zustand store생성

--- client/src/stores/sessionStore.ts
+++ client/src/stores/sessionStore.ts
@@ -1,14 +1,13 @@
-// authStore.js
-import create from "zustand";
+import create, { createStore } from "zustand";
+//typescript에서는 create 말고 createStore 사용.
+interface AuthStore {
+  isLoggedIn: boolean;
+  setLoggedIn: (value: boolean) => void;
+}
+//isLoggedIn 값을 관리 스토어 생성
+const useAuthStore = createStore<AuthStore>((set) => ({
+  isLoggedIn: false, // 초기값 false
+  setLoggedIn: (value) => set({ isLoggedIn: value }), // isLoggedIn 값을 설정하는 액션
+}));
 
-// interface UserState {
-//   isLoggedIn: boolean;
-//   setLoggedIn;
-// }
-// Zustand를 사용하여 전역 상태로 isLoggedIn 값을 관리하는 스토어 생성
-// const useAuthStore = create((set) => ({
-//   isLoggedIn: false, // 초기값은 false로 설정
-//   setLoggedIn: (value) => set({ isLoggedIn: value }), // isLoggedIn 값을 설정하는 액션
-// }));
-
-// export default useAuthStore;
+export default useAuthStore;
