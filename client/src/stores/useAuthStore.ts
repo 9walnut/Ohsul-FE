c1c2576c5d1d3c8b600d02aee72ec0b186872309
hyeoonjeoong <jeooooong22@gmail.com>
2024-02-24 13:54:54 +0900

    ✨ zustand persist 적용

--- client/src/stores/useAuthStore.ts
+++ client/src/stores/useAuthStore.ts
@@ -1,4 +1,5 @@
 import { createStore, StoreApi } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 type AuthStore = {
   isLoggedIn: boolean;
@@ -10,21 +11,30 @@ type AuthStore = {
   setUserNickname: (userNickname: string) => void;
 };
 
-// Zustand 스토어 생성
-const useAuthStore = createStore<AuthStore>((set) => ({
-  isLoggedIn: false,
-  login: () => set({ isLoggedIn: true }),
-  logout: () =>
-    set((state) => ({
+//Zustand persist
+const useAuthStore = createStore(
+  persist<AuthStore>(
+    (set) => ({
       isLoggedIn: false,
+      login: () => set({ isLoggedIn: true }),
+      logout: () =>
+        set((state) => ({
+          isLoggedIn: false,
+          userId: null,
+          userNickname: null,
+        })),
       userId: null,
+      setUserId: (userId: string) => set({ userId: userId }),
       userNickname: null,
-    })),
-  userId: null,
-  setUserId: (userId: string) => set({ userId: userId }),
-  userNickname: null,
-  setUserNickname: (userId: string) => set({ userId: userId }),
-}));
+      setUserNickname: (userNickname: string) =>
+        set({ userNickname: userNickname }),
+    }),
+    {
+      name: "userInfoStorage",
+    }
+  )
+  //)
+);
 
 export default useAuthStore;
 
@@ -35,12 +45,10 @@ export default useAuthStore;
 //2) useAuthStore의 상태(state) 가져오기 -> 콘솔로 true false 확인.
 //const isLoggedIn = useAuthStore.getState().isLoggedIn;
 
-//3)끝...
-
 //-------------------사용 할 때 [저장된 값 사용]
 //1) 상단에 store 불러오기
 // import useAuthStore from "../../stores/useAuthStore";
 
-//2) 사용하기 ...끝....
+//2) 사용하기
 //const userNickname = useAuthStore.getState().userNickname;
 // const { userNickname } = useAuthStore.getState();
