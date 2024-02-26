9385110e31b9b4fd764b8c785d365da76e0c8a99
hyeoonjeoong <jeooooong22@gmail.com>
2024-02-27 00:53:57 +0900

    ✨ MyReviewPage FetchData

--- client/src/pages/user/mypage/MyReviewPage.tsx
+++ client/src/pages/user/mypage/MyReviewPage.tsx
@@ -4,45 +4,46 @@ import * as S from "./MyReviewPageStyle";
 import axios from "axios";
 import useAuthStore from "../../../stores/useAuthStore";
 import BarReviewCard from "../../../components/common/BarReviewCard";
+import { CardBarReview } from "../../../types/Common";
 
 //DUMMY
-const reviewCount = 2;
-const DUMMYReview = [
-  {
-    barName: "언더그라운드",
-    userNickname: "졸린공룡",
-    score: 4,
-    barImg:
-      "https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzAyMDNfNjkg%2FMDAxNjc1MzU3OTAwMDc1.nDuEbsyEjQNKrN5JJn4PN7QN2himoQXkjdsOidYPEQ4g.hc5nBIjfzB85bNZRKiYcGhwY3ETdxAtLQUQhAi_hZ3cg.JPEG%2Fimage.jpg",
-    tag: {
-      drink: ["칵테일", "양주"],
-      mood: ["이야기 나누기 적당한"],
-      music: ["힙합"],
-    },
-    content:
-      "이러코저러코 리뷰리뷰리뷰리뷰리뷰 리뷰가 들어와여 리뷰리뷰리뷰리뷰 뷰가 들어와여 리뷰리뷰리뷰리뷰 리뷰가 들어와여 리뷰없으면 공백! 85자 이내 작성!",
-    date: "2024-02-22",
-  },
-  {
-    barName: "언더그라운드",
-    userNickname: "졸린공룡",
-    score: 2,
-    barImg:
-      "https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzAyMDNfNjkg%2FMDAxNjc1MzU3OTAwMDc1.nDuEbsyEjQNKrN5JJn4PN7QN2himoQXkjdsOidYPEQ4g.hc5nBIjfzB85bNZRKiYcGhwY3ETdxAtLQUQhAi_hZ3cg.JPEG%2Fimage.jpg",
-    tag: {
-      drink: ["칵테일", "양주"],
-      mood: ["다같이 즐기는"],
-      music: ["힙합"],
-    },
-    content: "이러코저러코 ",
-    date: "2024-02-26",
-  },
-];
+// const reviewCount = 2;
+// const DUMMYReview = [
+//   {
+//     barName: "언더그라운드",
+//     userNickname: "졸린공룡",
+//     score: 4,
+//     barImg:
+//       "https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzAyMDNfNjkg%2FMDAxNjc1MzU3OTAwMDc1.nDuEbsyEjQNKrN5JJn4PN7QN2himoQXkjdsOidYPEQ4g.hc5nBIjfzB85bNZRKiYcGhwY3ETdxAtLQUQhAi_hZ3cg.JPEG%2Fimage.jpg",
+//     tag: {
+//       drink: ["칵테일", "양주"],
+//       mood: ["이야기 나누기 적당한"],
+//       music: ["힙합"],
+//     },
+//     content:
+//       "이러코저러코 리뷰리뷰리뷰리뷰리뷰 리뷰가 들어와여 리뷰리뷰리뷰리뷰 뷰가 들어와여 리뷰리뷰리뷰리뷰 리뷰가 들어와여 리뷰없으면 공백! 85자 이내 작성!",
+//     date: "2024-02-22",
+//   },
+//   {
+//     barName: "언더그라운드",
+//     userNickname: "졸린공룡",
+//     score: 2,
+//     barImg:
+//       "https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzAyMDNfNjkg%2FMDAxNjc1MzU3OTAwMDc1.nDuEbsyEjQNKrN5JJn4PN7QN2himoQXkjdsOidYPEQ4g.hc5nBIjfzB85bNZRKiYcGhwY3ETdxAtLQUQhAi_hZ3cg.JPEG%2Fimage.jpg",
+//     tag: {
+//       drink: ["칵테일", "양주"],
+//       mood: ["다같이 즐기는"],
+//       music: ["힙합"],
+//     },
+//     content: "이러코저러코 ",
+//     date: "2024-02-26",
+//   },
+// ];
 
 const MyReviewPage = () => {
   const [isReview, setIsReview] = useState(true);
+  const [reviewData, setReviewData] = useState<CardBarReview[]>([]);
 
-  //✅ 어떻게 들어오냐
   useEffect(() => {
     const { userId } = useAuthStore.getState();
     const FetchData = async () => {
@@ -51,8 +52,13 @@ const MyReviewPage = () => {
           params: { userId },
         });
         if (res.status == 200) {
+          const { reviews, userNickname } = res.data;
+          setReviewData(reviews);
+
+          console.log("User nickname:", userNickname);
           console.log("MyReviewPage res: ", res);
           console.log("MyReviewPage res.data: ", res.data);
+          console.log("Review data:", reviewData);
         }
       } catch (error) {
         console.log("myReview render error : ", error);
@@ -63,18 +69,19 @@ const MyReviewPage = () => {
   return (
     <>
       <S.MyReviewPageLayout>
-        <S.ReviewCount>총 {reviewCount}개의 리뷰</S.ReviewCount>
+        <S.ReviewCount>총 {reviewData.length}개의 리뷰</S.ReviewCount>
+        <p> content, reviewId 만 들어옴</p>
         {isReview ? (
           <>
-            {DUMMYReview.map((content, index) => (
+            {reviewData.map((review, index) => (
               <BarReviewCard
                 key={index}
-                userNickname={content.userNickname}
-                score={content.score}
-                barImg={content.barImg}
-                tag={content.tag}
-                content={content.content}
-                date={content.date}
+                userNickname={review.userNickname}
+                score={review.score}
+                barImg={review.barImg}
+                tag={review.tag}
+                content={review.content}
+                date={review.date}
               />
             ))}
           </>
