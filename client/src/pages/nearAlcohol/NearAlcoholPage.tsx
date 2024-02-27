2706d8fb268729243310e7874d99612a80bd0001
dntjd129 <dntjd129@naver.com>
2024-02-27 15:58:10 +0900

    ♻️ map 요청 수정

--- client/src/pages/nearAlcohol/NearAlcoholPage.tsx
+++ client/src/pages/nearAlcohol/NearAlcoholPage.tsx
@@ -11,40 +11,20 @@ import KakaoMap07 from "../../components/common/KakaoMap07";
 
 import CardColReview from "../../components/common/CardColReview";
 
-const DUMMYBarReviewCard = {
-  userNickname: "졸린공룡",
-  score: 4,
-  barImg:
-    "https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzAyMDNfNjkg%2FMDAxNjc1MzU3OTAwMDc1.nDuEbsyEjQNKrN5JJn4PN7QN2himoQXkjdsOidYPEQ4g.hc5nBIjfzB85bNZRKiYcGhwY3ETdxAtLQUQhAi_hZ3cg.JPEG%2Fimage.jpg",
-  tag: {
-    drink: ["칵테일", "양주"],
-    mood: ["다같이 즐기는", "이야기 나누기 적당한"],
-    music: ["힙합"],
-  },
-  content:
-    "이러코저러코 리뷰리뷰리뷰리뷰리뷰 리뷰가 들어와여 리뷰리뷰리뷰리뷰 뷰가 들어와여 리뷰리뷰리뷰리뷰 리뷰가 들어와여 리뷰없으면 공백! 85자 이내 작성!",
-  date: "2024-02-22",
-};
-const DUMMYCardColTag = {
-  barId: 2,
-  barName: "언더그라운드",
-  score: 4,
-  barImg:
-    "https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzAyMDNfNjkg%2FMDAxNjc1MzU3OTAwMDc1.nDuEbsyEjQNKrN5JJn4PN7QN2himoQXkjdsOidYPEQ4g.hc5nBIjfzB85bNZRKiYcGhwY3ETdxAtLQUQhAi_hZ3cg.JPEG%2Fimage.jpg",
-  tag: {
-    drink: ["칵테일", "양주"],
-    mood: ["다같이 즐기는", "힙한"],
-    music: ["힙합"],
-  },
-};
-
 type CombinedData = (SearchResult | getBarInfo)[];
+
+interface BarDataTypes {
+  barName: string;
+  roadAddress: string;
+  telephone: string;
+}
 
 const NearAlcoholPage: React.FC = () => {
   const [viewMap, setViewMap] = useState(true);
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
   const [fetchBarData, setFetchBarData] = useState<SearchResult[]>([]);
-  const [barData, setBarData] = useState<CombinedData>([]);
+  // const [barData, setBarData] = useState<CombinedData>([]);
+  const [barInfo, setBarInfo] = useState([]);
 
   //view mode - 지도 보기 , 리스트 보기
   const handleViewChange = (newViewMap: boolean) => {
@@ -59,55 +39,46 @@ const NearAlcoholPage: React.FC = () => {
   useEffect(() => {
     console.log("주소들어옴", searchResults);
 
-    const phoneNumbers = searchResults
-      .filter((result) => result.phone)
-      .map((result) => result.phone.replace(/-/g, ""));
+    const barData: BarDataTypes[] = searchResults.map((result) => ({
+      barName: result.name,
+      roadAddress: result.address,
+      telephone: result.phone ? result.phone.replace(/-/g, "") : "",
+    }));
 
-    const barNames = searchResults
-      .filter((result) => result.name)
-      .map((result) => result.name);
-
-    console.log("번호 모음", phoneNumbers);
-    postStoreInfo(phoneNumbers, barNames);
+    postStoreInfo(barData);
   }, [searchResults]);
 
-  const postStoreInfo = async (phoneNumbers: string[], barNames: string[]) => {
-    const data = {
-      telephones: phoneNumbers,
-      barNames: barNames,
-    };
-    console.log("보내는 데이터임", data);
+  const postStoreInfo = async (barData: BarDataTypes[]) => {
+    console.log("보내는 데이터임", barData);
 
     try {
-      const res = await axios.post("/api/ohsul/near", data, {
+      const res = await axios.post("/api/ohsul/near", barData, {
         headers: {
           "Content-Type": "application/json",
         },
       });
       if (res.status === 200) {
-        const fetchData: any[] = res.data; // 응답 데이터 타입을 any 배열로 지정
+        setBarInfo(res.data);
 
-        // 검색 결과와 응답 데이터를 합친 배열을 생성
-        // 더미데이터에 없으면 barId 안들어옴 -> 데이터 보내면서 생성되고 barid 가져와야 될 듯
-        const combinedData = searchResults.map((searchResult) => {
-          const matchedData = fetchData.find(
-            (data) => data.barName === searchResult.name
-          );
-          if (matchedData) {
-            return {
-              ...searchResult,
-              barId: matchedData.barId,
-              barImg: matchedData.barImg,
-              alcoholTags: matchedData.alcoholTags,
-              moodTags: matchedData.moodTags,
-              musicTags: matchedData.musicTags,
-            };
-          }
-          return searchResult;
-        });
-        setBarData(combinedData);
-
-        console.log("combinedData: ", combinedData);
+        // // 검색 결과와 응답 데이터를 합친 배열을 생성
+        // // 더미데이터에 없으면 barId 안들어옴 -> 데이터 보내면서 생성되고 barid 가져와야 될 듯
+        // const combinedData = searchResults.map((searchResult) => {
+        //   const matchedData = fetchData.find(
+        //     (data) => data.barName === searchResult.name
+        //   );
+        //   if (matchedData) {
+        //     return {
+        //       ...searchResult,
+        //       barId: matchedData.barId,
+        //       barImg: matchedData.barImg,
+        //       alcoholTags: matchedData.alcoholTags,
+        //       moodTags: matchedData.moodTags,
+        //       musicTags: matchedData.musicTags,
+        //     };
+        //   }
+        //   return searchResult;
+        // });
+        // setBarData(combinedData);
         console.log("barData: ", barData);
       }
     } catch (error) {
@@ -121,52 +92,23 @@ const NearAlcoholPage: React.FC = () => {
 
   return (
     <>
-      <PageLayout>
-        <Header title="내 주변의 술" />
-        <KakaoMap07 onSearchResults={handleSearchResults} />
-        {barData &&
-          barData.map((result, index) => (
-            <CardColTag
-              key={index}
-              barName={result.name}
-              barPhone={handleBarPhone(result.phone)}
-              barId={result.barId}
-            />
-          ))}
-        {/* {barData &&
-          barData.map((result) => {
-            if ("barId" in result) {
-              return (
-                <CardColTag
-                  key={result.barId}
-                  barName={result.name}
-                  barPhone={handleBarPhone(result.phone)}
-                />
-              );
-            } else {
-              return null; // SearchResult 타입인 경우 처리
-            }
-          })} */}
-        {/* {searchResults.map((result, index) => (
-          <CardColReview
-            barName={result.name}
+      <Header title="내 주변의 술" />
+      <KakaoMap07 onSearchResults={handleSearchResults} />
+      {barInfo &&
+        barInfo.map((result, index) => (
+          <CardColTag
             key={index}
+            // @ts-ignore
+            barName={result.name}
+            // @ts-ignore
             barPhone={handleBarPhone(result.phone)}
+            // @ts-ignore
+            barId={result.barId}
           />
-        ))} */}
-        {/* <CardColTag
-          barId={DUMMYCardColTag.barId}
-          barName={DUMMYCardColTag.barName}
-          score={DUMMYCardColTag.score}
-          barImg={DUMMYCardColTag.barImg}
-          tag={DUMMYCardColTag.tag}
-        /> */}
-        {/* <p>얘는 가로형 카드 리뷰형</p>
-        <CardColReview barName="언더그라운드" /> */}
+        ))}
 
-        {viewMap ? <p>지도 보기 컴포넌트</p> : <p>리스트 보기 컴포넌트</p>}
-        <Toggle2 viewMap={viewMap} onViewChange={handleViewChange} />
-      </PageLayout>
+      {viewMap ? <p>지도 보기 컴포넌트</p> : <p>리스트 보기 컴포넌트</p>}
+      {/* <Toggle2 viewMap={viewMap} onViewChange={handleViewChange} /> */}
     </>
   );
 };
@@ -174,8 +116,8 @@ const NearAlcoholPage: React.FC = () => {
 export default NearAlcoholPage;
 
 const PageLayout = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: center; */
 `;
--- client/src/pages/searchAlcohol/SearchAlcoholPage.tsx
+++ client/src/pages/searchAlcohol/SearchAlcoholPage.tsx
@@ -8,12 +8,6 @@ import CardColReview from "../../components/common/CardColReview";
 import axios from "axios";
 
 interface BarDataTypes {
-  barName: string[];
-  roadAddress: string[];
-  telephone: string[];
-}
-
-interface TransformedSearchResult {
   barName: string;
   roadAddress: string;
   telephone: string;
@@ -21,34 +15,26 @@ interface TransformedSearchResult {
 
 const SearchAlcoholPage: React.FC = () => {
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
-  const [BarInfo, setBarInfo] = useState([]);
+  const [barInfo, setBarInfo] = useState([]);
 
   // 이 지역 재검색 클릭 시
   useEffect(() => {
     console.log("주소들어옴", searchResults);
 
-    // const barData: BarDataTypes = {
-    //   barName: searchResults.map((result) => result.name),
-    //   roadAddress: searchResults.map((result) => result.address),
-    //   telephone: searchResults.map((result) =>
-    //     result.phone ? result.phone.replace(/-/g, "") : ""
-    //   ),
-    // };
-
-    const transformedData: BarDataTypes[] = searchResults.map((result) => ({
-      barName: [result.name],
-      roadAddress: [result.address],
-      telephone: result.phone ? [result.phone.replace(/-/g, "")] : [""],
+    const barData: BarDataTypes[] = searchResults.map((result) => ({
+      barName: result.name,
+      roadAddress: result.address,
+      telephone: result.phone ? result.phone.replace(/-/g, "") : "",
     }));
 
-    postStoreInfo(transformedData); // API 호출 시, 객체를 배열로 감싸서 보내기
+    postStoreInfo(barData); // API 호출 시, 객체를 배열로 감싸서 보내기
   }, [searchResults]);
 
-  const postStoreInfo = async (transformedData: BarDataTypes[]) => {
-    console.log("보내는 데이터임", transformedData);
+  const postStoreInfo = async (barData: BarDataTypes[]) => {
+    console.log("보내는 데이터임", barData);
 
     try {
-      const res = await axios.post("/api/ohsul/near", transformedData, {
+      const res = await axios.post("/api/ohsul/near", barData, {
         headers: {
           "Content-Type": "application/json",
         },
@@ -74,6 +60,10 @@ const SearchAlcoholPage: React.FC = () => {
     setSearchResults(results);
   };
 
+  const handleBarPhone = (phone: string) => {
+    return phone.replace(/-/g, "");
+  };
+
   //오술태그 선택된 값 넣기
   const DUMMYTags = {
     alcohol: ["alcohol_1", "alcohol_2", "alcohol_5"],
@@ -90,7 +80,7 @@ const SearchAlcoholPage: React.FC = () => {
       <TagBox checkedTags={DUMMYTags} />
       {/* dot Img */}
       <img src="/assets/images/border_dot.png" alt="border_dot" />
-      {BarInfo.map((result, index) => (
+      {barInfo.map((result, index) => (
         <CardColReview
           // @ts-ignore
           barId={result.barId}
