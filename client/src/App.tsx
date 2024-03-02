cfb59168462525d2f86da01bfcfc4407e6194cb8
dntjd129 <dntjd129@naver.com>
2024-03-02 13:20:58 +0900

    README

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,11 +12,14 @@ const App: React.FC = () => {
     <>
       <ThemeProvider theme={theme}>
         <CenterLayout>
-          <MainLayout>
-            <Outlet />
-            {/* Outlet에 Router.tsx에서 매칭시켜둔 element가 렌더링됨*/}
-          </MainLayout>
-          <Navbar />
+          <CenterTextWrapper>
+            {/* <CenterText>오늘의 술🍺</CenterText> */}
+            <MainLayout>
+              <Outlet />
+              {/* Outlet에 Router.tsx에서 매칭시켜둔 element가 렌더링됨*/}
+            </MainLayout>
+            <Navbar />
+          </CenterTextWrapper>
         </CenterLayout>
       </ThemeProvider>
     </>
@@ -30,7 +33,7 @@ const MainLayout = styled.div`
   height: 100%;
   min-height: 932px;
   @media screen and (min-height: 933px) {
-    height: 100vh;
+    height: 100vmax;
   }
 
   background-color: ${({ theme }) => theme.colors.bgColor};
@@ -39,13 +42,36 @@ const MainLayout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.9) 0px 5px 18px -7px;
 `;
 
 const CenterLayout = styled.div`
+  /* background: linear-gradient(
+    91.89978272135795deg,
+    rgba(239, 225, 210, 1) 21.958428939918665%,
+    rgba(219, 182, 140, 1) 74.17679426918728%
+  ); */
+
+  background-image: url("/assets/images/background_beige.jpg");
+
   display: flex;
   justify-content: center;
   align-items: flex-start;
   text-align: center;
+`;
+
+const CenterTextWrapper = styled.div`
+  /* position: relative; */
+`;
+
+const CenterText = styled.div`
+  position: absolute;
+  font-size: 120px;
+  left: 200px;
+  top: 20%;
+  font-family: ${({ theme }) => theme.fonts.ydFont};
+  color: ${({ theme }) => theme.colors.darkFont};
+  z-index: 20;
 `;
 
 export default App;
